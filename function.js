dbc0b8be040f01ef4a1ee35ef94097d4047c6152
Izufox77 <izufox77@gmail.com>
2021-09-22 15:34:46 +0200

    first commit

---
+++ function.js
@@ -0,0 +1,22 @@
+//Imports
+var opn = require('opn');
+
+//Functions
+
+function openLink(link){
+    opn(link);
+}
+
+function createCharacters(length){
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   exports.resultCharacters = result;
+   return result;
+}
+
+exports.openLink = openLink;
+exports.createCharacters = createCharacters;
---
+++ test.js
@@ -0,0 +1,5 @@
+const IzuSimplify = require('./function')
+
+//IzuSimplify.openLink('https://google.com')
+IzuSimplify.createCharacters(5)
+console.log(IzuSimplify.resultCharacters)
